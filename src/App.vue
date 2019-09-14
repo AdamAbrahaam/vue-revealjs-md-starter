6bcfd60595b05be8c86f241b5a0ab2fcdf6b0d6b
Adam Abrahám <adam.shadow77@hotmail.com>
2019-09-14 11:33:22 +0200

    reveal md

--- src/App.vue
+++ src/App.vue
@@ -1,28 +1,59 @@
 <template>
   <div id="app">
-    <img alt="Vue logo" src="./assets/logo.png" />
-    <HelloWorld msg="Welcome to Your Vue.js App" />
+    <div class="reveal">
+      <div class="slides">
+        <section><foo /></section> <!-- With vue-markdown-loader (loader config in vue.config.js)-->
+        <section><div v-html="compiledMarkdown"></div></section> <!-- With marked js -->
+        <section>
+          <section>Vertical Slide 1</section>
+          <section>Vertical Slide 2</section>
+        </section>
+      </div>
+    </div>
   </div>
 </template>
 
 <script>
-import HelloWorld from "./components/HelloWorld.vue";
+import Reveal from 'reveal.js/js/reveal'
+import Foo from "./components/foo.md"
+
+let marked = require('marked');
 
 export default {
-  name: "app",
+  name: 'app',
   components: {
-    HelloWorld
+    Foo
+  },
+
+  data() {
+    return {
+      text: "# asd"
+    }
+  },
+
+  computed: {
+    compiledMarkdown: function () {
+      return marked(this.text)
+    }
+  },
+
+  mounted() {
+    Reveal.initialize()
   }
-};
+}
 </script>
 
 <style>
+@import url('../node_modules/reveal.js/css/reveal.css');
+@import url('../node_modules/reveal.js/css/theme/white.css');
+
 #app {
-  font-family: "Avenir", Helvetica, Arial, sans-serif;
+  font-family: 'Avenir', Helvetica, Arial, sans-serif;
   -webkit-font-smoothing: antialiased;
   -moz-osx-font-smoothing: grayscale;
   text-align: center;
   color: #2c3e50;
-  margin-top: 60px;
+  /* margin-top: 60px; */
+  height: 100vh;
 }
 </style>
